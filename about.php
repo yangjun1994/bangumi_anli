37377733e32878e3e6bcad9dbcd4a0ab03636b7b
YangJun <yangjun@1994.io>
2017-02-02 00:36:01 +0800

    开坑。。。数据库以外的内容先都commit上去吧。。。

---
+++ about.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+    <head>
+        <meta charset="utf-8">
+            <meta content="IE=edge" http-equiv="X-UA-Compatible">
+                <meta content="width=device-width, initial-scale=1" name="viewport">
+                    <title>
+                        About - Bangumi Anli Plan α
+                    </title>
+                    <link href="https://lib.1994.io/bootstrap/bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet">
+                    </link>
+                    <link href="core.css" rel="stylesheet">
+                    </link>
+                </meta>
+            </meta>
+        </meta>
+    </head>
+    <body class="color-about">
+        <nav class="navbar navbar-inverse navbar-fixed-top">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button aria-expanded="false" class="navbar-toggle collapsed" data-target="#bs-example-navbar-collapse-1" data-toggle="collapse" type="button">
+                        <span class="sr-only">
+                            Toggle navigation
+                        </span>
+                        <span class="icon-bar">
+                        </span>
+                        <span class="icon-bar">
+                        </span>
+                        <span class="icon-bar">
+                        </span>
+                    </button>
+                    <a class="navbar-brand" href="index.php">
+                        Bangumi Anli Plan α
+                    </a>
+                </div>
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                        <li>
+                            <a href="index.php">
+                                Home
+                            </a>
+                        </li>
+                        <li class="active">
+                            <a href="about.php">
+                                About
+                                <span class="sr-only">
+                                    (current)
+                                </span>
+                            </a>
+                        </li>
+                        <li class="dropdown">
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <div class="container-fluid">
+            <h1>
+                About
+            </h1>
+            <h2>
+                为毛写这么个网站？
+            </h2>
+            <p>
+                高考(2012)后入坑二次元，已经沉迷不能自拔，每天都会有大量时间放在看番上，姑且算看过挺多的番了。因为有时有刚入坑的朋友让我推荐一些番看，于是考虑做这样一个个人安利向网站，一方面希望能够给刚入坑的朋友们一些建（安）议（利），另一方面我也能够更好地学习前端技术。
+                <br/>
+                （这个难看的要死的网页请勿吐槽，我前端新手。）
+            </p>
+            <h2>
+                你这逗比是谁？
+            </h2>
+            <p>
+                简单写下自我介绍（请轻吐槽(=・ω・=)）：
+                <br/>
+                1994年出生的单身狗。单身历史=年龄。汪汪汪！欢迎勾搭。
+                <br/>
+                高考后被某高中同学安利入坑二次元。从此不能自拔。
+                <br/>
+                LLer。被朋友喊去帮忙做LL相关内容，然后接触LL，然后又不能自拔了。。。欢迎访问查卡器
+                <a href="https://c.1994.io">
+                    戳这里
+                </a>
+                <br/>
+                现在基本只看百合番，希望变成女孩子和其他女孩子抱抱(/ω＼)。再次欢迎勾搭。
+                <br/>
+                <span class="heimu-about" title="(=・ω・=)">
+                    可爱的蓝孩子，性别认同似乎不正常（不考虑吃糖什么的），180cm+的偶（经）尔（常）在宿舍女装的Hentai，再次再次欢迎勾搭。
+                </span>
+                <br/>
+                <br/>
+                我不擅长写自我介绍。好像最后一般都会附个履历表吧：
+                <br/>
+                幼儿园：身体原因，基本不去。天天在家乱拆东西。
+                <br/>
+                小学：玩电脑，很爱哭(/ω＼)，体育不及格。
+                <br/>
+                初中：地理极差，中考手滑gg。
+                <br/>
+                高中：除一次机打卡写歪了外，全部考试总成绩全校第一(<ゝω·)kira☆~。理综和文综（分文理前）全部考试全校第一。学校唯一特批不用上晚自习的人(=・ω・=)。
+                <br/>
+                本科：@BJUT，又开始玩玩玩。然后
+                <span class="heimu-about" title="(=・ω・=)">
+                    名额多到和发大米似的
+                </span>
+                推免时，被某室友安利到了中科院某实验室。
+                <br/>
+                现在在中国科学院读PhD。
+                <br/>
+                <br/>
+                联系方式:
+                <br/>
+                电邮: askme#1994.io（请自行替换#为@）
+                <br/>
+                Blog:
+                <a href="https://blog.1994.io">
+                    blog.1994.io
+                </a>
+                <br/>
+                Steam:
+                <a href="https://steamcommunity.com/id/lagrenge/">
+                    Lagrenge
+                </a>
+                <br/>
+                <br/>
+            </p>
+        </div>
+        <div class="container-fluid">
+            <footer class="center">
+                Lagrenge 2017-2333
+            </footer>
+        </div>
+        <script src="https://lib.1994.io/jquery/jquery-3.1.1.min.js">
+        </script>
+        <script src="https://lib.1994.io/bootstrap/bootstrap-3.3.7-dist/js/bootstrap.min.js">
+        </script>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+    <head>
+        <meta charset="utf-8">
+            <meta content="IE=edge" http-equiv="X-UA-Compatible">
+                <meta content="width=device-width, initial-scale=1" name="viewport">
+                    <title>
+                        Bangumi Anli Plan α
+                    </title>
+                    <link href="https://lib.1994.io/bootstrap/bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet">
+                    </link>
+                    <link href="core.css" rel="stylesheet">
+                    </link>
+                </meta>
+            </meta>
+        </meta>
+    </head>
+    <body class="color-main">
+        <nav class="navbar navbar-inverse navbar-fixed-top">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button aria-expanded="false" class="navbar-toggle collapsed" data-target="#bs-example-navbar-collapse-1" data-toggle="collapse" type="button">
+                        <span class="sr-only">
+                            Toggle navigation
+                        </span>
+                        <span class="icon-bar">
+                        </span>
+                        <span class="icon-bar">
+                        </span>
+                        <span class="icon-bar">
+                        </span>
+                    </button>
+                    <a class="navbar-brand" href="index.php">
+                        Bangumi Anli Plan α
+                    </a>
+                </div>
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                        <li class="active">
+                            <a href="index.php">
+                                Home
+                                <span class="sr-only">
+                                    (current)
+                                </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="about.php">
+                                About
+                            </a>
+                        </li>
+                        <li class="dropdown">
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <div class="container-fluid main-title">
+            <div class="alert alert-danger lowpadmargin" role="alert">
+                本网页的推荐为“个人向”，本死宅日常弃番。
+            </div>
+            <div class="alert alert-info lowpadmargin" role="alert">
+                我大概会推荐大量“百合”，“日常”番。同时会同时包含“治愈”与“致郁”番，请仔细甄别。
+            </div>
+            <h1 class="lowpadmargin">
+                Bangumi Anli Plan α
+            </h1>
+            <h2 class="lowpadmargin">
+                Lagrenge的番组安利站
+            </h2>
+        </div>
+        <div class="container-fluid">
+            <table class="table table-bordered main-table-striped">
+                <tr>
+                    <th>
+                        番名
+                    </th>
+                    <th>
+                        一句话介绍
+                    </th>
+                    <th>
+                        个人喜爱程度
+                    </th>
+                    <th>
+                        年份
+                    </th>
+                    <th>
+                        春夏秋冬
+                    </th>
+                    <th>
+                        逸站连接
+                    </th>
+                    <th>
+                        是否能百合大法好（我没救了）
+                    </th>
+                    <th>
+                        详情页（还没糊好）
+                    </th>
+                </tr>
+
+<?php
+include('conn.php');
+$sql = "SELECT id, name, year, time, intro, b_link, star, yuri FROM bangumi ORDER BY year DESC, time, star DESC";
+$result = mysqli_query ($conn, $sql);
+$star_str=array("","☆","☆☆","☆☆☆","☆☆☆☆","☆☆☆☆☆");
+$time_str=array("","冬季新番（1月番）","春季新番（4月番）","夏季新番（7月番）","秋季新番（10月番）");
+$yuri_str=array("没有百合QAQ","轻微百合元素（可能勉强）","轻百合","百合");
+function get_b_link($b1) {
+    if ($b1 != Null) {
+        return "<a href='".$b1."'>戳我进入</a>";
+    }
+    else return "无";
+}
+while($bangumi_row = mysqli_fetch_array($result)) {
+    echo "<tr>";
+    echo "<td>" . $bangumi_row['name'] . "</td>";
+    echo "<td>" . $bangumi_row['intro'] . "</td>";
+    echo "<td>" . $star_str[$bangumi_row['star']] . "</td>";
+    echo "<td>" . $bangumi_row['year'] . "</td>";
+    echo "<td>" . $time_str[$bangumi_row['time']] . "</td>";
+    echo "<td>" . get_b_link($bangumi_row['b_link']) . "</td>";
+    echo "<td>" . $yuri_str[$bangumi_row['yuri']] . "</td>";
+     echo "<td>" . "假装这里有按钮id=".$bangumi_row['id'] . "</td>";
+    echo "</tr>";
+    }
+mysqli_close($conn);
+?>
+
+</table>
+</div>
+<div class="container-fluid">
+    <footer class="center">
+        Lagrenge 2017-2333
+    </footer>
+</div>
+<script src="https://lib.1994.io/jquery/jquery-3.1.1.min.js">
+</script>
+<script src="https://lib.1994.io/bootstrap/bootstrap-3.3.7-dist/js/bootstrap.min.js">
+</script>
